d7d2cd8722389959c9d9b0aa93e7be933095b0bc
donunobunjo <don.unobunjo@gmail.com>
2020-04-24 12:21:08 +0100

    working on database.php

--- config/database.php
+++ config/database.php
@@ -1,11 +1,12 @@
 <?php
 
 use Illuminate\Support\Str;
-$url = parse_url(getenv("DATABASE_URL"));
+/*$test = getenv("CLEARDB_DATABASE_URL");
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $host = $url["host"] ?? null;
 $username = $url["user"] ?? null;
 $password = $url["pass"] ?? null;
-$database = substr($url["path"], 1);
+$database = substr($url["path"], 1);*/
 
 return [
 
@@ -53,14 +54,18 @@ return [
             //'url' => env('DATABASE_URL'),
             'url' => $url,
             //'host' => env('DB_HOST', '127.0.0.1'),
-            'host' => $host,
+            //'host' => $host,
+            'host' => getenv("DATABASE_URL") ? parse_url(getenv("CLEARDB_DATABASE_URL"))["host"] : '',
             'port' => env('DB_PORT', '3306'),
             //'database' => env('DB_DATABASE', 'forge'),
-            'database' => $database,
+            //'database' => $database,
+            'database' => getenv("DATABASE_URL") ? substr(parse_url(getenv("CLEARDB_DATABASE_URL"))["path"], 1) : '',
             //'username' => env('DB_USERNAME', 'forge'),
-            'username' => $username,
+            //'username' => $username,
+            'username' => getenv("DATABASE_URL") ? parse_url(getenv("CLEARDB_DATABASE_URL"))["user"] : '',
             //'password' => env('DB_PASSWORD', ''),
-            'password' => $password,
+            //'password' => $password,
+            'password' => getenv("DATABASE_URL") ? parse_url(getenv("CLEARDB_DATABASE_URL"))["pass"] : '',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
